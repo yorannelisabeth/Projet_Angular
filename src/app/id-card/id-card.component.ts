cece829b4e480fdc142b2aa2079d5ef3940efad0
Yorann Elisabeth <yorannelisabeth@gmail.com>
2019-03-26 13:41:04 +0100

    Add angular files for work!

---
+++ src/app/id-card/id-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-id-card',
+  templateUrl: './id-card.component.html',
+  styleUrls: ['./id-card.component.css']
+})
+export class IdCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
